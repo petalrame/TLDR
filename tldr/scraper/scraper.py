aaebfac8e729d8e2b283071e3fe8f34fad49a0fb
SamuelBWasserman <sambwasserman@gmail.com>
2017-02-12 20:06:37 -0500

    Using scrapy to get titles

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -1,8 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib2
+import scrapy
 
-def scrapeNYtimes:
-    url = "https://www.nytimes.com/"
-    content = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(content)
-    
+class NewsSpider(scrapy.Spider):
+    name = 'newsspider'
+    start_urls = ['https://www.nytimes.com']
+
+    def parse(self, response):
+        for title in response.css('h2 a'):
+            yield {'title': title.css('a ::text').extract_first()}
+
+        next_page = response.css('h2 a ::attr(href)').extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)

9717920d404214d271f0ec277a8c30a238b9210f
SamuelBWasserman <sambwasserman@gmail.com>
2017-02-16 18:00:25 -0500

    Minor bug fixes & changes

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -1,31 +1,44 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
+import csv
 def crawlForSites():
+    newsSites = csv.writer(open('newsSites.csv', 'w', newline=''))
     url = "https://en.wikipedia.org/wiki/Wikipedia:News_sources/Americas"
     urlsToVisit = [url] #Stack of urls to that may contain
     visited = [url] #list of sites already visited
-    print ("hello")
     while (urlsToVisit):
         try:
             ogHtml = urllib.request.urlopen(url).read()
         except:
             print("shit")
         soup = BeautifulSoup(ogHtml)
-        urls.pop(0)
+        urlsToVisit.pop(0)
 
         for tag in soup.findAll('a', href = True):
             foundUrl = tag['href']
-            foundUrl = urlparse.urljoin(url , foundUrl) #If it finds an href with a link that will give problems, this makes it a working link
-            print(foundUrl)
+            #print(foundUrl)
+            newsSites.writerow( foundUrl)
             if url in foundUrl and foundUrl not in visited:
                 urls.append(foundUrl) #Adding new site to queue
                 visited.append(foundUrl) #Adding new site to visited
 
-    def main():
-        print("hello")
+def crawlArticles():
+    with open('newsSites.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        website = ''
+        for row in reader:
+            for x in row:
+                website = website + x
+            print(website+"\n")
+            website = ''
+            #Scrape some shiz
+
+
+
+def main():
         crawlForSites();
+        crawlArticles();
         return
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         main()

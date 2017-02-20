904004f2c2b1435ac0bb109c24b38f68198e5827
Sam Wasserman <Sam Wasserman>
2017-02-20 23:37:38 +0000

    Progress stops while running idk why csv is empty now

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -4,6 +4,7 @@ import csv
 from bs4 import BeautifulSoup
 
 
+
 def crawl_for_sites():
     """Crawl the internet for websites"""
     news_sites = csv.writer(open('newsSites.csv', 'w', newline=''))
@@ -12,19 +13,28 @@ def crawl_for_sites():
     visited = [url] #list of sites already visited
     while urls_to_visit:
         try:
-            og_html = urllib.request.urlopen(url).read()
+            #print(urls_to_visit[0])
+            og_html = urllib.request.urlopen(urls_to_visit[0]).read()
         except:
             print("shit")
-        soup = BeautifulSoup(og_html)
-        urls_to_visit.pop(0)
+        current_site = urls_to_visit.pop(0)
+        soup = BeautifulSoup(og_html,"html.parser")
+        titles = soup.findAll('title')
+        should_we_search = 0 #Added this in an attempt to decrease # of websites we crawl. Progress kinda stops when executing
 
-        for tag in soup.findAll('a', href=True):
-            found_url = tag['href']
-            #print(foundUrl)
-            news_sites.writerow(found_url)
-            if url in found_url and found_url not in visited:
-                urls_to_visit.append(found_url) #Adding new site to queue
-                visited.append(found_url) #Adding new site to visited
+        for title in titles:
+            content = ("".join(title.contents)).lower()
+            if "news" in content:
+                print(content)
+                news_sites.writerow(current_site)
+                should_we_search = 1
+        if should_we_search == 1:
+            for tag in soup.findAll('a', href=True):
+                found_url = tag['href']
+                #print(found_url)
+                if found_url not in visited:
+                    urls_to_visit.append(found_url) #Adding new site to queue
+                    visited.append(found_url) #Adding new site to visited
 
 
 def crawl_articles():
@@ -44,7 +54,7 @@ def crawl_articles():
 def main():
     """Call the other methods"""
     crawl_for_sites()
-    crawl_articles()
+    #crawl_articles()
     return
 
 if __name__ == "__main__":

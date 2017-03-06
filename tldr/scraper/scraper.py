8aa19086d8d228a0f08d8fb4114e0093760ace36
SamuelBWasserman <sambwasserman@gmail.com>
2017-03-06 15:05:30 -0500

    Used tld to get base domain in an effort to reduce repeating domains...At least we dont get articles anymore

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -2,39 +2,53 @@
 import urllib.request
 import csv
 from bs4 import BeautifulSoup
-
+import urllib.parse
+# Package that "Extracts the top level domain (TLD) from the URL given"
+from tld import get_tld
+from tld.utils import update_tld_names
 
 
 def crawl_for_sites():
+    update_tld_names()
     """Crawl the internet for websites"""
     news_sites = csv.writer(open('newsSites.csv', 'w', newline=''))
-    url = "https://en.wikipedia.org/wiki/Wikipedia:News_sources/Americas"
-    urls_to_visit = [url] #Stack of urls to that may contain
-    visited = [url] #list of sites already visited
+    # Stack of urls to that may contain
+    urls_to_visit = ['https://www.yahoo.com/news/','https://news.google.com/','http://www.foxnews.com/','https://www.washingtonpost.com/',
+                    'www.bbc.co.uk/news','https://www.nytimes.com/']
     while urls_to_visit:
         try:
-            #print(urls_to_visit[0])
+            # print(urls_to_visit[0])
             og_html = urllib.request.urlopen(urls_to_visit[0]).read()
         except:
-            print("shit")
-        current_site = urls_to_visit.pop(0)
-        soup = BeautifulSoup(og_html,"html.parser")
-        titles = soup.findAll('title')
-        should_we_search = 0 #Added this in an attempt to decrease # of websites we crawl. Progress kinda stops when executing
+            print("URL can not be opened")
 
+        current_site = urls_to_visit.pop(0)
+        # Make BeautifulSoup Object from webpage
+        soup = BeautifulSoup(og_html, "html.parser")
+
+        # We want only sites with news in the title
+        titles = soup.findAll('title')
         for title in titles:
             content = ("".join(title.contents)).lower()
             if "news" in content:
-                print(content)
                 news_sites.writerow(current_site)
-                should_we_search = 1
-        if should_we_search == 1:
-            for tag in soup.findAll('a', href=True):
-                found_url = tag['href']
-                #print(found_url)
-                if found_url not in visited:
-                    urls_to_visit.append(found_url) #Adding new site to queue
-                    visited.append(found_url) #Adding new site to visited
+                # Add new sites found on page to urls_to_visit
+        for tag in soup.findAll('a', href=True):
+            found_url = tag['href']
+            try:
+                domain = get_tld(found_url)
+                # Variable to count occurances of current website in
+                count = 0
+                for website in urls_to_visit:
+                    if domain in website:
+                        # Stop repeating domains
+                        count = count + 1
+
+                if count == 0:
+                    urls_to_visit.append(domain)
+
+            except:
+                print("")
 
 
 def crawl_articles():
@@ -45,17 +59,17 @@ def crawl_articles():
         for row in reader:
             for i in row:
                 website = website + i
-            print(website+"\n")
+            print(website + "\n")
             website = ''
-            #Scrape some shiz
-
+            # Scrape some shiz
 
 
 def main():
     """Call the other methods"""
     crawl_for_sites()
-    #crawl_articles()
+    crawl_articles()
     return
+
 
 if __name__ == "__main__":
     main()

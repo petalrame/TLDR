6d27fe4855fcac53080fffa22e894845d8349b5c
SamuelBWasserman <sambwasserman@gmail.com>
2017-03-10 16:17:27 -0500

    Article Crawler started. Problem with urlib raising an exception

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -13,11 +13,11 @@ def crawl_for_sites():
     update_tld_names()
     news_sites = csv.writer(open('newsSites.csv', 'w', newline=''))
     # Stack of urls to that may contain
-    urls_to_visit = ['https://www.yahoo.com/news/','https://news.google.com/','http://www.foxnews.com/','https://www.washingtonpost.com/',
-                    'www.bbc.co.uk/news','https://www.nytimes.com/']
-    #TODO
+    urls_to_visit = ['https://www.yahoo.com/news/', 'https://news.google.com/', 'http://www.foxnews.com/', 'https://www.washingtonpost.com/',
+                     'www.bbc.co.uk/news', 'https://www.nytimes.com/']
+    # TODO
     # Still getting some duplicates
-
+    # BBC tld raises an exception
     while len(urls_to_visit) < 500:
         try:
             # print(urls_to_visit[0])
@@ -29,8 +29,9 @@ def crawl_for_sites():
         # Make BeautifulSoup Object from webpage
         soup = BeautifulSoup(og_html, "html.parser")
 
-        #Checks for news in meta description tags and will not fall for social media
-        desc = soup.findAll(attrs={"name":"description"})
+        # Checks for news in meta description tags and will not fall for social
+        # media
+        desc = soup.findAll(attrs={"name": "description"})
         try:
             desc = desc[0]['content']
             if 'news' in desc.lower() and 'twitter' not in desc.lower() and 'facebook' not in desc.lower() and 'instagram' not in desc.lower():
@@ -62,20 +63,48 @@ def crawl_for_sites():
 
 def crawl_articles():
     """Crawls through different articles in a domain"""
+    urls_to_visit = []
+    update_tld_names()
+    articles = csv.writer(open('articles.csv', 'w', newline=''))
     with open('newsSites.csv') as csvfile:
         reader = csv.reader(csvfile)
         website = ''
         for row in reader:
             for i in row:
                 website = website + i
-            print(website + "\n")
+            # Add website to queue
+            urls_to_visit.append(website)
+            print(website)
+            # Reset website
             website = ''
-            # Scrape some shiz
-
+    while(urls_to_visit):
+        # Make a soup of the website domain
+        try:
+            og_html = urllib.request.urlopen(urls_to_visit[0]).read()
+        except:
+            print("URL can not be opened")
+        current_site = urls_to_visit.pop(0)
+        # Make BeautifulSoup Object from webpage
+        try:
+            soup = BeautifulSoup(og_html, "html.parser")
+        except:
+            continue
+        desc = soup.findAll(attrs={"name": "aplicationName"})
+        try:
+            desc = desc[0]['content'].lower()
+            if 'article' in desc:
+                print(desc)
+                articles.writerow()
+        except:
+            pass
+        #Put all hrefs in urls_to_visit
+        for tag in soup.findAll('a', href=True):
+            found_url = tag['href']
+            urls_to_visit.append(found_url)
 
 def main():
     """Call the other methods"""
-    crawl_for_sites()
+    #crawl_for_sites()
     crawl_articles()
     return
 

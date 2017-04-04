ea474e4ea2498a37b979f8aa254bb3476803b145
eminkin97 <eminkin99@gmail.com>
2017-04-03 20:16:50 -0400

    Added Background Image to UI

--- clientsidetldr/src/App.js
+++ clientsidetldr/src/App.js
@@ -5,12 +5,22 @@ import Article from './Components/Article';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div style={divStyle} className="App">
 		<Header />
 		<Article />
       </div>
     );
   }
+}
+
+const imgUrl = "http://www.planwallpaper.com/static/images/colorful-triangles-background_yB0qTG6.jpg"
+
+const divStyle = {
+	backgroundImage: 'url(' + imgUrl + ')',
+	backgroundSize: '100% 100%',
+	position: 'absolute',
+	width: '100%',
+	height: '100%'
 }
 
 export default App;

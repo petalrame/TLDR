05e4fee0b9bb1d5f48b233445e232317ceaf7380
SamuelBWasserman <sambwasserman@gmail.com>
2017-05-18 15:37:48 -0400

    Made downloads async

--- tldr/scraper/scraper3.py
+++ tldr/scraper/scraper3.py
@@ -1,25 +1,32 @@
 import newspaper
+#news_pool async library
+from newspaper import news_pool
 
 def scrape():
     """Uses the Newspaper library to parse news websites for content"""
     #Master list
     articles = []
-    #USA Today
+    #Build Papers
     usa = newspaper.build('https://www.usatoday.com/',memoize_articles=False)
+    nytimes = newspaper.build('http://nytimes.com',memoize_articles=False)
+    wsj = newspaper.build('http://wsj.com',memoize_articles=False)
+
+    papers = [slate_paper, tc_paper, espn_paper]
+    news_pool.set(papers, threads_per_source=2) # (3*2) = 6 threads total
+    news_pool.join()
+    
+    #USA Today
     for article in usa.articles:
-        article.download()
         article.parse()
         #Article object init
         article_obj = dict()
         #Fill the object
-        article_obj.update({'title':article.title})
+        article_obj.update({'title':article.title})new
         article_obj.update({'body':article.text})
         article_obj.update({'author':article.authors})
         articles.append(article_obj)
     #NY Times
-    nytimes = newspaper.build('http://nytimes.com',memoize_articles=False)
     for article in nytimes.articles:
-        article.download()
         article.parse()
         #Article object init
         article_obj = dict()
@@ -29,9 +36,7 @@ def scrape():
         article_obj.update({'author':article.authors})
         articles.append(article_obj)
     #Washington Street Journal
-    wsj = newspaper.build('http://wsj.com',memoize_articles=False)
     for article in wsj.articles:
-        article.download()
         article.parse()
         #Article object init
         article_obj = dict()

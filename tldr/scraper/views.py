0e7edca46b2c260757dbc73550fcd29ca12002ae
SamuelBWasserman <sambwasserman@gmail.com>
2017-06-25 14:39:41 -0400

    Made skeleton views

--- tldr/scraper/views.py
+++ tldr/scraper/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tldr index.")
+
+"""This function fetches information from the database"""
+def dbGet(request):
+    return null
+
+"""This function posts data to the database"""
+def dbPost(request):
+    return null

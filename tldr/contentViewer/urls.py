67e1ce79701d8c8c4abd639ee1573d100f45ecc5
eminkin97 <eminkin99@gmail.com>
2017-08-05 20:53:32 -0400

    Created Event Serializer. Created User Interaction Viewset with 2 basic views. More views to be added soon. Created Unit tests for user interaction viewset

--- tldr/contentViewer/urls.py
+++ tldr/contentViewer/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register(r'api', views.UserInteractionsViewSet, base_name="user_interaction")
+
 urlpatterns = [
-	url(r'^api/$', views.dbGet, name="getAllData"),
+	url(r'^', include(router.urls)),
 	url(r'^[a-zA-Z0-9]*$', views.loadSite, name="index"),
 ]
--- tldr/contentViewer/views.py
+++ tldr/contentViewer/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
 
 def loadSite(request):
 	return render(request, 'contentViewer/index.html')
-	#return HttpResponse("Ram Sucks")
 
-"""This function fetches information from the database"""
-def dbGet(request):
-	data = Event.objects.all()
-	return JsonResponse({'entries': list(data)})
+class UserInteractionsViewSet(viewsets.ViewSet):
+	"""
+	ViewSet for retreiving data from Event Table. Updating
+	like counter in Event Table.
+	"""
 
-"""This function posts data to the database"""
-def dbPost(request):
-    return null
+	#Gets most popular data in table based on ranking
+	@list_route(methods=['GET'])
+	def most_popular(self, request):
+		data = Event.objects.order_by('ranking')	#Gets data based on numLikes
+		serializer = EventSerializer(data, many=True)		#Serialize data
+
+		return Response(serializer.data, content_type='json')	#Return JSON serialized data
+
+	#Gets most viewed data, data with most clicktraffic
+	@list_route(methods=['GET'])
+	def most_viewed(self, request):
+		data = Event.objects.order_by('clicktraffic').reverse()	
+		serializer = EventSerializer(data, many=True)		
+
+		return Response(serializer.data, content_type='json')

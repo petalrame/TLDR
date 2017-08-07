56993da558571be60c67ae5c1565d82f0d7367fa
Sam Wasserman <sambwasserman@gmail.com>
2017-08-07 10:50:15 -0400

    Fixed scraper iteration

--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -1,41 +1,49 @@
 """Newspaper is a library for extracting & curating articles."""
 import newspaper
-# news_pool async library
 from newspaper import news_pool
-# Master list
-articles = []
+# List of article dictionaries.
+# Dicts must contain Author, Url, Body text and datetime added
+article_list = []
 
 
 def initialize_scraper():
-    """Use the Newspaper library to parse news websites for data."""
+    """Build newspaper objects and parse and download them."""
     # Build Papers
     fox = newspaper.build('https://www.foxnews.com/', memoize_articles=False)
     nytimes = newspaper.build('http://nytimes.com', memoize_articles=False)
     wsj = newspaper.build('http://wsj.com', memoize_articles=False)
+    bbc = newspaper.build('http://bbcnews.com', memoize_articles=False)
+    cnn = newspaper.build('http://cnn.com', memoize_articles=False)
+    breit = newspaper.build('http://breitbart.com', memoize_articles=False)
 
-    papers = [fox, nytimes, wsj]
+    papers = [fox, nytimes, wsj, bbc, cnn, breit]
     news_pool.set(papers, threads_per_source=2)  # (3*2) = 6 threads total
     news_pool.join()
-    scrape(fox)
+    for source in papers:
+        for article in source.articles:
+            scrape(article)
 
 
-def scrape(source):
-    """Collect article information. Takes a source object as a parameter."""
-    for article in source.articles:
-        article.parse()
-        # Dictionary that contains article information
-        article_obj = dict()
-        # Populate the dict with fields from articles
-        article_obj.update({'title': article.title, 'body': article.text,
-                            'author': article.authors})
-        articles.append(article_obj)
-        print(article.title)
+def scrape(article):
+    """Scrape the source article."""
+    # article.download()
+    # article.parse()
+    print(article.authors)
+    article_obj = dict()
+    article_obj.update({'title': article.title,
+                        'body': article.text,
+                        'author': article.authors,
+                        'datetime': ""})
+
+    article_list.append(article_obj)
 
 
-def main():
-    """Run scrapy.py this is called."""
-    initialize_scraper()
+def display_data():
+    """Output the scraper results to console."""
+    for article in article_list:
+        for key in article:
+            print(article[key])
 
 
 if __name__ == "__main__":
-    main()
+    initialize_scraper()

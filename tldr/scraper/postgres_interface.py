c718c56c5f1e8a0dd36054822e658f9a939b84f5
Sam Wasserman <sambwasserman@gmail.com>
2017-08-13 20:19:08 -0400

    Completed interface for insertion into the article table

--- tldr/scraper/postgres_interface.py
+++ tldr/scraper/postgres_interface.py
@@ -1,6 +1,26 @@
 """Provides an inteface for adding data to the article_object table."""
 import psycopg2
+conn = None
 
-# Connect to the table containing article_objects in article_database
-conn = psycopg2.connect(database="article_database", user = "samwasserman",
-                        password = "#Dowhile97")
+
+def connect():
+    """Connect to article_database."""
+    # Connect to the table containing article_objects in article_database
+    try:
+        conn = psycopg2.connect("dbname='article_database'\
+                                user='samwasserman'\
+                                host='localhost' password='#Dowhile97'")
+        print("Connected")
+    except:
+        print "I am unable to connect to the database"
+
+
+def insert(id, authors, url, content):
+    """Insert data into column."""
+    cur = conn.cursor()
+    cur.execute("INSERT INTO \"article_objects\" \
+                VALUES (" + str(id) + ",'" + authors + "','"
+                + url + "','" + content + "');")
+    conn.commit()
+    print ("Records created successfully")
+    conn.close()

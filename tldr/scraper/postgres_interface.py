4e3ef363ed9a4f54c38ee9ba671037f3ffccee44
Sam Wasserman <sambwasserman@gmail.com>
2017-08-15 21:54:02 -0400

    Article db row insertion

--- tldr/scraper/postgres_interface.py
+++ tldr/scraper/postgres_interface.py
@@ -1,26 +1,16 @@
 """Provides an interface for adding data to the article_object table."""
 import psycopg2
-conn = None
+connection_str = "dbname='article_database'\
+                        user='samwasserman'\
+                        host='localhost' password='#Dowhile97'"
+conn = psycopg2.connect(connection_str)
 
 
-def connect():
-    """Connect to article_database."""
-    # Connect to the table containing article_objects in article_database
-    try:
-        conn = psycopg2.connect("dbname='article_database'\
-                                user='samwasserman'\
-                                host='localhost' password='#Dowhile97'")
-        print("Connected")
-    except:
-        print "I am unable to connect to the database"
-
-
-def insert(id, authors, url, content):
+def insert(authors, url, content):
     """Insert data into column. PARAMETERS: (unique id for postgres, etc.)"""
+    print("Inserting into DB")
     cur = conn.cursor()
-    cur.execute("INSERT INTO \"article_objects\" \
-                VALUES (" + str(id) + ",'" + authors + "','"
-                + url + "','" + content + "');")
+    cur.execute("INSERT INTO article_objects VALUES(" + "\'" + authors + "','" + url + "','" + content + "')");
     conn.commit()
     print ("Records created successfully")
     conn.close()
--- tldr/scraper/scraper.py
+++ tldr/scraper/scraper.py
@@ -5,7 +5,6 @@ import postgres_interface
 # List of article dictionaries.
 # Dicts must contain Author, Url, Body text and datetime added
 article_list = []
-TIME_INTERVAL = 1
 
 
 def get_source_list():
@@ -13,12 +12,12 @@ def get_source_list():
     # Build Papers Objects to be downloaded and parsed for data extraction.
     tech_crunch = newspaper.build(
         'https://www.techcrunch.com/', memoize_articles=False)
-    fox = newspaper.build('https://www.foxnews.com/', memoize_articles=False)
-    nytimes = newspaper.build('http://nytimes.com', memoize_articles=False)
-    wsj = newspaper.build('http://wsj.com', memoize_articles=False)
-    bbc = newspaper.build('http://bbcnews.com', memoize_articles=False)
-    cnn = newspaper.build('http://cnn.com', memoize_articles=False)
-    breit = newspaper.build('http://breitbart.com', memoize_articles=False)
+    fox = newspaper.build('https://www.foxnews.com/', memoize_articles=True)
+    nytimes = newspaper.build('http://nytimes.com', memoize_articles=True)
+    wsj = newspaper.build('http://wsj.com', memoize_articles=True)
+    bbc = newspaper.build('http://bbcnews.com', memoize_articles=True)
+    cnn = newspaper.build('http://cnn.com', memoize_articles=True)
+    breit = newspaper.build('http://breitbart.com', memoize_articles=True)
 
     papers = [tech_crunch, fox, nytimes, wsj, bbc, cnn, breit]
     return papers
@@ -30,14 +29,9 @@ def scrape(sources):
         for article in source.articles:
             article.download()
             article.parse()
-            article_obj = dict()
-            article_obj.update({'title': article.title,
-                                'body': article.text,
-                                'author': article.authors,
-                                'datetime': datetime.datetime.now()})
-            postgres_interface.insert
-            (1, article.authors, article.url, article.text)
-        article_list.append(article_obj)
+            print(article.title)
+            postgres_interface.connect()
+            postgres_interface.insert(6, ''.join(article.authors), ''.join(article.url), ''.join(article.text))
 
 
 def display_data():
@@ -53,8 +47,7 @@ def run_scraper():
     postgres_interface.connect()
     sources = get_source_list()
     scrape(sources)
-    display_data()
-    print("The internet has been thoroughly scraped.")
+    print("Scraper has completed.")
 
 
 if __name__ == "__main__":

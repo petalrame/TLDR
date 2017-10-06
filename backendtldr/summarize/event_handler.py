531cece0ac5938a0a8e2fad21296068e078928ec
Sam Wasserman <sambwasserman@gmail.com>
2017-10-06 17:34:47 -0400

    generate events from article

--- backendtldr/summarize/event_handler.py
+++ backendtldr/summarize/event_handler.py
@@ -3,8 +3,15 @@ Handles the creation/updating of events.
 Pairs articles from the Article table to an Event.
 Finds Events needing summarization and passes it to the Summary Handler
 """
+from django.utils import timezone
 from summarize.models import Event
+from scraper.models import Article
 
-
-# STATUS: Blocked
-# TODO: Pair single articles to an event and create Event objects.
+def generate_events_from_articles():
+    """Creates an empty event object with fields taken from article objects."""
+    # Loop through all the articles in the article table.
+    for article in Article.objects.all():
+        # article = e is questionable
+        e = Event(event_title = e.title, summaries = None,  articles = e, no_tags = None,
+                  tags = None, last_updated = timezone.now(), needs_summary = True)
+        e.save()

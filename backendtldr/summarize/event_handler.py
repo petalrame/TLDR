4ca43b29ea411b48f847684e275f1b2ebcd9202f
Sam Wasserman <sambwasserman@gmail.com>
2017-10-10 00:18:25 -0400

    Set the event_id of articles to its event

--- backendtldr/summarize/event_handler.py
+++ backendtldr/summarize/event_handler.py
@@ -11,7 +11,10 @@ def generate_events_from_articles():
     """Creates an empty event object with fields taken from article objects."""
     # Loop through all the articles in the article table.
     for article in Article.objects.all():
-        # article = e is questionable
-        e = Event(event_title = e.title, summaries = None,  articles = e, no_tags = None,
+        # Create an event object with attributes from the article
+        e = Event(event_title = article.title, summaries = None,  articles = article, no_tags = None,
                   tags = None, last_updated = timezone.now(), needs_summary = True)
+        # Save the data to the database
         e.save()
+        # Set the event_id of the article to the event it was just created from
+        article.event_id = e.id
